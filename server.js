2bc239180bbf4a81d59ef0d918b0f07c0950d2f3
Joseph Sazon <jsazon427@gmail.com>
2020-07-07 21:03:44 +0800

    add users routes

--- server.js
+++ server.js
@@ -6,6 +6,9 @@ app.get('/', (req, res) =>
   res.json({ msg: 'Welcome to the ContactKeeper API.' })
 );
 
+// Define Routes
+app.use('/api/users', require('./routes/users'));
+
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
