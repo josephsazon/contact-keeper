0ac52d912953205db0ecd934e73a05a621d2bdbc
Joseph <jsazon427@gmail.com>
2020-07-14 21:20:06 +0800

    add get contacts functionality

--- routes/users.js
+++ routes/users.js
@@ -1,10 +1,9 @@
 const express = require('express');
+const router = express.Router();
 const { check, validationResult } = require('express-validator');
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 const config = require('config');
-
-const router = express.Router();
 
 const User = require('../models/User');
 
