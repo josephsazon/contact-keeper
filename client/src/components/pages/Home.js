a4f974a98dfeb46fbdf4b710ff3af780343b77fa
Joseph <jsazon427@gmail.com>
2020-07-15 21:56:03 +0800

    feat: client/components/contacts - add ContactForm to Home page

--- client/src/components/pages/Home.js
+++ client/src/components/pages/Home.js
@@ -1,12 +1,15 @@
 import React from 'react';
 
 // components
+import ContactForm from '../contacts/ContactForm';
 import Contacts from '../contacts/Contacts';
 
 const Home = () => {
   return (
     <div className="grid-2">
-      <div>{/* Contact Form */}</div>
+      <div>
+        <ContactForm />
+      </div>
       <div>
         <Contacts />
       </div>
