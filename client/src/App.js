6cabd73557de06a5e4e2c3392abdf5ca957e578a
Joseph <jsazon427@gmail.com>
2020-07-16 21:26:52 +0800

    feat: client/app - add alerts component and state to app.js

--- client/src/App.js
+++ client/src/App.js
@@ -2,11 +2,13 @@ import React, { Fragment } from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 // state
+import AlertState from './context/alert/AlertState';
 import AuthState from './context/auth/AuthState';
 import ContactState from './context/contact/ContactState';
 
 // components
 import About from './components/pages/About';
+import Alerts from './components/layout/Alerts';
 import Home from './components/pages/Home';
 import Login from './components/auth/Login';
 import Navbar from './components/layout/Navbar';
@@ -19,19 +21,22 @@ const App = () => {
   return (
     <AuthState>
       <ContactState>
-        <Router>
-          <Fragment>
-            <Navbar />
-            <div className="container">
-              <Switch>
-                <Route exact path="/" component={Home} />
-                <Route exact path="/about" component={About} />
-                <Route exact path="/login" component={Login} />
-                <Route exact path="/register" component={Register} />
-              </Switch>
-            </div>
-          </Fragment>
-        </Router>
+        <AlertState>
+          <Router>
+            <Fragment>
+              <Navbar />
+              <div className="container">
+                <Alerts />
+                <Switch>
+                  <Route exact path="/" component={Home} />
+                  <Route exact path="/about" component={About} />
+                  <Route exact path="/login" component={Login} />
+                  <Route exact path="/register" component={Register} />
+                </Switch>
+              </div>
+            </Fragment>
+          </Router>
+        </AlertState>
       </ContactState>
     </AuthState>
   );
