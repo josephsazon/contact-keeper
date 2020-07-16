3c7a7f7ef9372fc7333873654f9440a5032b3f2a
Joseph <jsazon427@gmail.com>
2020-07-16 20:48:55 +0800

    feat: client/components/auth - add login functionality and route

--- client/src/App.js
+++ client/src/App.js
@@ -8,6 +8,7 @@ import ContactState from './context/contact/ContactState';
 // components
 import About from './components/pages/About';
 import Home from './components/pages/Home';
+import Login from './components/auth/Login';
 import Navbar from './components/layout/Navbar';
 import Register from './components/auth/Register';
 
@@ -25,6 +26,7 @@ const App = () => {
               <Switch>
                 <Route exact path="/" component={Home} />
                 <Route exact path="/about" component={About} />
+                <Route exact path="/login" component={Login} />
                 <Route exact path="/register" component={Register} />
               </Switch>
             </div>
