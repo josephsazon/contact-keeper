435d7e9440ddc9ed98f3982520189f78315eba7c
Joseph <jsazon427@gmail.com>
2020-07-18 17:35:03 +0800

    chore: prepare deployment

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ app.use('/api/users', require('./routes/users'));
 // Serve static assets in production
 if (process.env.NODE_ENV === 'production') {
   // Set static folder
-  app.use(express.statoc('clien/build'));
+  app.use(express.static('client/build'));
 
   app.get('*', (req, res) =>
     res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
