6e5fd2d67927c03d2af919c3828a2ae9f7aea994
Joseph <jsazon427@gmail.com>
2020-07-18 13:28:33 +0800

    feat: client/components/navbar - refactor links to authLinks and guestLinks

--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -1,27 +1,48 @@
-import React from 'react';
+import React, { Fragment, useContext } from 'react';
 import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
 
+import AuthContext from '../../context/auth/authContext';
+
 const Navbar = ({ icon, title }) => {
+  const authContext = useContext(AuthContext);
+
+  const { isAuthenticated, user } = authContext;
+
+  const onLogout = () => {
+    authContext.logout();
+  };
+
+  const authLinks = (
+    <Fragment>
+      <li>Hello, {user && user.name}</li>
+      <li>
+        <a onClick={onLogout} href="#!">
+          <i className="fas fa-sign-out-alt"></i>{' '}
+          <span className="hide-sa">Logout</span>
+        </a>
+      </li>
+    </Fragment>
+  );
+  const guestLinks = (
+    <Fragment>
+      <li>
+        <Link to="/login">Login</Link>
+      </li>
+      <li>
+        <Link to="/register">Register</Link>
+      </li>
+    </Fragment>
+  );
+
   return (
     <div className="navbar bg-primary">
-      <h1>
-        <i className={icon} /> {title}
-      </h1>
-      <ul>
-        <li>
-          <Link to="/">Home</Link>
-        </li>
-        <li>
-          <Link to="/about">About</Link>
-        </li>
-        <li>
-          <Link to="/Login">Login</Link>
-        </li>
-        <li>
-          <Link to="/register">Register</Link>
-        </li>
-      </ul>
+      <Link to="/">
+        <h1>
+          <i className={icon} /> {title}
+        </h1>
+      </Link>
+      <ul>{isAuthenticated ? authLinks : guestLinks}</ul>
     </div>
   );
 };
